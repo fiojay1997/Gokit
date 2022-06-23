1aef412c2e33785efd763ae4e1d473c223bf0964
Kaijie Fu <u1314550@utah.edu>
2022-06-23 05:36:01 -0600

    added router and changed some structure in engine

--- web/engine/engine.go
+++ web/engine/engine.go
@@ -1,30 +1,28 @@
 package engine
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandleFunc defines the return type in HTTP ServeHTTP
-type HandleFunc func(http.ResponseWriter, *http.Request)
+type HandleFunc func(*Context)
 
 // Engine defines the core of the framework
 // Implementation of ServeHTTP method, this engine will take over all
 // the request from native HTTP package
 type Engine struct {
-	router map[string]HandleFunc
+	router *router
 }
 
-// New gets a plain engine with no configuration 
+// New gets a plain engine with no configuration
 func New() *Engine {
-	return &Engine{router: make(map[string]HandleFunc)}
+	return &Engine{router: newRouter()}
 }
 
 // addRoute is used for adding request patterns and methods to the engine
 // TODO: this can be chained
 func (e *Engine) addRoute(method, pattern string, handler HandleFunc) {
-	key := method + "-" + pattern
-	e.router[key] = handler
+	e.router.addRoute(method, pattern, handler)
 }
 
 // GET adds GET request to the engine
@@ -40,26 +38,21 @@ func (e *Engine) POST(pattern string, handler HandleFunc) {
 // PUT adds PUT request to the engine
 func (e *Engine) PUT(pattern string, handler HandleFunc) {
 	e.addRoute("PUT", pattern, handler)
-} 
+}
 
 // DELETE adds DELETE request to the engine
 func (e *Engine) DELETE(pattern string, handler HandleFunc) {
 	e.addRoute("PUT", pattern, handler)
-} 
+}
 
 // ServeHTTP overrides the function in HTTP interface
 // This takes over the control of interceptor in HTTP package
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := e.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := NewContext(w, req)
+	e.router.handle(c)
 }
 
 // Run executes the engine with the mapped routes and methods
 func (e *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, e)
 }
-
