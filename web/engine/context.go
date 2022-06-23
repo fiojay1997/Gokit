51608d86397e02bf34fdee2da567f387630838eb
Kaijie Fu <u1314550@utah.edu>
2022-06-23 05:31:17 -0600

    added context and updated something in engine

---
+++ web/engine/context.go
@@ -0,0 +1,74 @@
+package engine
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer     http.ResponseWriter
+	Request    *http.Request
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func NewContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer:  w,
+		Request: req,
+		Path:    req.URL.Path,
+		Method:  req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Request.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Request.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	sb := strings.Builder{}
+	sb.WriteString(format)
+	for _, v := range values {
+		sb.WriteString(v.(string))
+	}
+	c.Writer.Write([]byte(sb.String()))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
